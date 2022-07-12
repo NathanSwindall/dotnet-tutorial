28c8520626522d6593810640b65dea1b4e4ad4bd
nswindall <nswindall@colsa.com>
2022-07-12 07:45:21 -0500

    Fixed the react skeleton

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -26,29 +26,6 @@ function App() {
             </List.Item>
         ))}
       </List>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {activities.map((activity: any )=> (
-          <li key={activity.id}>
-            {activity.title}
-          </li>
-        ))}
-        {ducks.map(duck => (
-          <DuckItem duck={duck} key={duck.name}/>
-        ))}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
